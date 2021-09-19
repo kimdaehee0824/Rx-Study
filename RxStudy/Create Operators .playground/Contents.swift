a25c0934b2e5802c1ccf25f0f7e6ad9603693009
김대희 <0824dh@naver.com>
2021-09-19 20:28:11 +0900

    generate 학습

--- RxStudy/Create Operators .playground/Contents.swift
+++ RxStudy/Create Operators .playground/Contents.swift
@@ -9,7 +9,7 @@ let iPhone = "📱"
 let cdBox = "💽"
 let products = ["📱", "⌚️", "🖱", "⌨️", "🖨"]
 
-    // Just
+// Just
 print("\n--------------------\nJust")
 
 Observable.just(element)    // return Observable
@@ -21,15 +21,15 @@ Observable.just([1, 2, 3, 4])   // 문자열을 그대로 방출. from 과 혼�
     .subscribe { event in print(event)}
     .disposed(by: disposeBag)
 
-    // 문자열을 방출하는 Observable
+// 문자열을 방출하는 Observable
 
 /*
  public static func just(_ element: Element) -> Observable<Element> {
-     Just(element: element)
+ Just(element: element)
  }
  */
 
-    // of
+// of
 print("\n--------------------\nof")
 
 Observable.of(iPhone, cdBox, keyBoard)  // 여러개 전달 가능
@@ -43,26 +43,26 @@ Observable.of([1, 2], [3, 4], [5, 6])
 
 /*
  public static func of(_ elements: Element ..., scheduler: ImmediateSchedulerType = CurrentThreadScheduler.instance) -> Observable<Element> {
-     ObservableSequence(elements: elements, scheduler: scheduler)
+ ObservableSequence(elements: elements, scheduler: scheduler)
  }
  */
 
-    // from
+// from
 print("\n--------------------\nfrom")
 
 
 Observable.from(products)   // products는 배열, of에서 사용 불가
     .subscribe { event in print(event)}
     .disposed(by: disposeBag)
-    // 배열에 포함된 요소를 하나씩 방출
+// 배열에 포함된 요소를 하나씩 방출
 
 /*
  public static func from(_ array: [Element], scheduler: ImmediateSchedulerType = CurrentThreadScheduler.instance) -> Observable<Element> {
-     ObservableSequence(elements: array, scheduler: scheduler)
+ ObservableSequence(elements: array, scheduler: scheduler)
  }
  */
 
-    // range
+// range
 
 print("\n--------------------\nrange")
 
@@ -72,6 +72,30 @@ Observable.range(start: 1, count: 5)    //시작부터 start count번까지 1씩
 
 /*
  public static func range(start: Element, count: Element, scheduler: ImmediateSchedulerType = CurrentThreadScheduler.instance) -> Observable<Element> {
-     RangeProducer<Element>(start: start, count: count, scheduler: scheduler)
+ RangeProducer<Element>(start: start, count: count, scheduler: scheduler)
+ }
+ */
+
+// generate
+print("\n--------------------\ngenerate")
+
+Observable.generate(initialState: 0, condition: {$0 <= 10}, iterate: {$0 + 2})
+    .subscribe { print($0)}
+    .disposed(by: disposeBag)
+
+Observable.generate(initialState: iPhone, condition: {$0.count <= 10}, iterate: {
+    $0.count.isMultiple(of: 2) ? $0 + iPhone : $0 + cdBox
+})
+    .subscribe { print($0)}
+    .disposed(by: disposeBag)
+
+/*
+ initialState : 시작점, condition : true를 전달해야 작동, false 누르면 complete 후 종료
+ iterate : 값을 바꾸는 거 (증가, 감소 등)
+ 
+ */
+/*
+ public static func generate(initialState: Element, condition: @escaping (Element) throws -> Bool, scheduler: ImmediateSchedulerType = CurrentThreadScheduler.instance, iterate: @escaping (Element) throws -> Element) -> Observable<Element> {
+ Generate(initialState: initialState, condition: condition, iterate: iterate, resultSelector: { $0 }, scheduler: scheduler)
  }
  */
