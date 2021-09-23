f0d0a8e400dbc5c308d74362f069397d008cbd91
김대희 <0824dh@naver.com>
2021-09-23 15:53:39 +0900

    element 학습

--- RxStudy/Filtering Operators.playground/Contents.swift
+++ RxStudy/Filtering Operators.playground/Contents.swift
@@ -4,11 +4,22 @@ import RxSwift
 let disposeBag = DisposeBag()
 let fruits = ["🍏", "🍐", "🍊", "🍋", "🍌", "🍉"]
 
-    // ignoreElements
+// ignoreElements
 print("\n--------------------\nignoreElements\n")
 
 Observable.from(fruits)
-    .ignoreElements()   // next 같은 거는 전달되지 않고 Completed, Error 메세지가 전딜됨
-    // next가 방출은 되고 있지만 전달되지 않음
+    .ignoreElements()
+// next 같은 거는 전달되지 않고 Completed, Error 메세지가 전딜됨
+// next가 방출은 되고 있지만 전달되지 않음
+    .subscribe {print($0)}
+    .disposed(by: disposeBag)
+
+// element
+print("\n--------------------\nelementAt\n")
+
+Observable.from(fruits)
+    .element(at: 2) // 현재는 element 권장
+// 정수를 받아 Observable 리턴
+// 제한적으로 방출
     .subscribe {print($0)}
     .disposed(by: disposeBag)
