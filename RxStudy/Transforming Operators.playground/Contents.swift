170bd14f6d11488905d8f96fcd35c3fbab8113f8
김대희 <0824dh@naver.com>
2021-09-24 20:05:17 +0900

    flatMap 학습

--- RxStudy/Transforming Operators.playground/Contents.swift
+++ RxStudy/Transforming Operators.playground/Contents.swift
@@ -25,10 +25,26 @@ print("\n--------------------\nmap\n")
 
 Observable.from(skills)
 //    .map { "hello \($0)" }
-    .map { $0.count }   // array의 길이 추가
+    .map { $0.count }   // array의
     .subscribe { print($0)}
     .disposed(by: disposeBag)
 
 // 원본 옵저버블이 방출하는 요소를 대상으로 함수를 실행하고 결과를 새로운 옵저버블로 리턴하는 map
 
+// flatMap
+print("\n--------------------\nflatMap\n")
 
+let a = BehaviorSubject(value: 1)
+let b = BehaviorSubject(value: 2)
+let c = BehaviorSubject(value: 4)
+
+let subject2 = PublishSubject<BehaviorSubject<Int>>()
+subject2
+    .flatMap { $0.asObservable() }  // asObservable는 subject를 Observable로 변환
+    .subscribe { print($0) }
+    .disposed(by: disposeBag)
+
+subject2.onNext(a)
+subject2.onNext(b)  // 값이 바뀌면 자동으로 변경
+subject2.onNext(c)
+// network 연결할 떄 자주 사용
