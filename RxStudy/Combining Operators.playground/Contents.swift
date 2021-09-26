d7c3008669d98bb9e984e5bb698241f4be05eec6
김대희 <0824dh@naver.com>
2021-09-26 22:36:50 +0900

    combineLatest  학습

--- RxStudy/Combining Operators.playground/Contents.swift
+++ RxStudy/Combining Operators.playground/Contents.swift
@@ -4,6 +4,10 @@ import RxSwift
 let bag = DisposeBag()
 let numbers = [1, 2, 3, 4, 5]
 
+enum MyError : Error {
+    case error
+}
+
 // startWith
 print("\n-------------------------\nstartWith\n")
 
@@ -13,4 +17,32 @@ Observable.from(numbers)
 // 가변 파라미터, Stack같은 느낌, 후입선출
     .subscribe { print($0) }
     .disposed(by: bag)
+
+// combineLatest
+print("\n-------------------------\ncombineLatest\n")
+
+let languangs = PublishSubject<String>()
+let greetings = PublishSubject<String>()
+
+Observable.combineLatest(greetings, languangs) { rth, lth -> String in
+    return "\(rth) \(lth)"
+}
+.subscribe { print($0) }
+.disposed(by: bag)
+
+greetings.onNext("hi")  // 이 상태에서는 languangs가 안나와서 방출이 안됨
+languangs.onNext("world!")
+
+greetings.onNext("Hello")   // 전에 꺼랑 알아서 조합해서 맘ㄴ들어줌
+languangs.onNext("RxSwift!")
+
+greetings.onCompleted()
+
+greetings.onError(MyError.error)
+languangs.onError(MyError.error)    // onError가 두개 모두 있어야 되네.....
+
+
+languangs.onNext("SwiftUI!")    // languangs가 onCompleted되어있지 않기 때문에 next 전달됨
+
+languangs.onCompleted() // 이러면 onCompleted됨
 
