367fd8e030b047d8aa4c5da0930c634936ac8978
김대희 <0824dh@naver.com>
2021-10-13 22:13:09 +0900

    [ADD] StaticLocationProvider 설정

--- RxWeather/RxWeather/Service/LocationProviderType.swift
+++ RxWeather/RxWeather/Service/LocationProviderType.swift
@@ -3,3 +3,12 @@
 import Foundation
 import CoreLocation
 import RxSwift
+
+protocol LocationProviderType {
+    @discardableResult
+    func currentLocation() -> Observable<CLLocation>// 현제 위치가 필요할 떄
+    
+    @discardableResult
+    func currentAddress() -> Observable<String>
+    
+}
--- RxWeather/RxWeather/Service/StaticLocationProvider.swift
+++ RxWeather/RxWeather/Service/StaticLocationProvider.swift
@@ -3,3 +3,15 @@
 import Foundation
 import CoreLocation
 import RxSwift
+
+struct StaticLocationProvider: LocationProviderType {
+    
+    func currentLocation() -> Observable<CLLocation> {
+        return Observable.just(CLLocation.gangnamStation)
+    }
+
+    func currentAddress() -> Observable<String> {
+        return Observable.just("강남역")
+    }
+
+}
